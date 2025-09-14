bce7de6398f269a1844bc500617e19139c9e07c3
Hannes Uhlin Collmo <148004074+hanino87@users.noreply.github.com>
2025-09-14 18:19:48 +0200

    added function to generate productname

--- labs/06_07_08_test_framework/libs/utils.py
+++ labs/06_07_08_test_framework/libs/utils.py
@@ -28,3 +28,17 @@ def generate_password_with_prefix(prefix: str = "pass", length: int = 12) -> str
     """
     random_part = ''.join(random.choices(string.ascii_lowercase + string.digits, k=length))
     return f"{prefix}_{random_part}"
+
+def generate_product_with_prefix(prefix: str = "product", length: int = 10) -> str:
+    """
+    Generate a string with a configurable prefix and a random string.
+
+    Args:
+        prefix (str): The prefix for the username (default: "user").
+        length (int): Length of the random string to append (default: 8).
+
+    Returns:
+        str: A username like "user_ab12cd34".
+    """
+    random_part = ''.join(random.choices(string.ascii_lowercase + string.digits, k=length))
+    return f"{prefix}_{random_part}"
