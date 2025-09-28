4b4a12f88db07f3df46c7471a52082919ad0c853
Hannes Uhlin Collmo <148004074+hanino87@users.noreply.github.com>
2025-09-28 18:09:42 +0200

    update my api.js file

--- application/frontend/src/api.js
+++ application/frontend/src/api.js
@@ -1,4 +1,7 @@
-const API_URL = "http://localhost:8000"
+//By default it will point to the container's url. so it will work only in jenkins.
+// If you want to try it locally, then you need to re deploy but pointing to localhost instead.
+const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
+
 
 export async function api(path, method = 'GET', body = null, token = null) {
   const headers = { 'Content-Type': 'application/json' }
