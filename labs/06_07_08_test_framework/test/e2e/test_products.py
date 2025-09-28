86f7d5296f3879b194c19839c46671a87e2a4845
Hannes Uhlin Collmo <148004074+hanino87@users.noreply.github.com>
2025-09-28 22:08:56 +0200

    test

--- labs/06_07_08_test_framework/test/e2e/test_products.py
+++ labs/06_07_08_test_framework/test/e2e/test_products.py
@@ -14,7 +14,7 @@ def test_add_product_to_catalog(page: Page):
     expect(fish_locator).to_be_visible()
 
     total_products = len(admin_page.get_current_product_count())
-    assert total_products == 2, f"Expected 2 products, got {total_products}"
+    assert total_products == 1, f"Expected 1 products, got {total_products}"
     print("✅ Product added. Total products:", total_products)
 
 def test_remove_product_from_catalog(page: Page):
@@ -26,12 +26,12 @@ def test_remove_product_from_catalog(page: Page):
     expect(fish_locator).to_be_hidden()
 
     total_products = len(admin_page.get_current_product_count())
-    assert total_products == 1, f"Expected 1 product, got {total_products}"
+    assert total_products == 0, f"Expected 1 product, got {total_products}"
     
     empty_basket_message = admin_page.no_products_header_text
     expect(empty_basket_message).to_be_hidden()
     if empty_basket_message.is_hidden():
-        print("✅ Product is gone. Total products: 1")
+        print("✅ Product is gone. Total products: 0")
     else:
         print("❌ Empty basket message visible unexpectedly.")
 
