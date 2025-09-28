522a2e682b13af4d85883e33e5152ca0f99bb36b
Hannes Uhlin Collmo <148004074+hanino87@users.noreply.github.com>
2025-09-28 21:34:02 +0200

    update my config vite file to make it more robust for allowed host

--- application/frontend/vite.config.js
+++ application/frontend/vite.config.js
@@ -2,5 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  server: {
+    host: true,
+    port: 5173,
+    allowedHosts: ['app-frontend', 'localhost'] // ✅ allow both local + Jenkins
+  }
 })
