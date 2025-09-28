0c089385fa5338ca6702b83e35d21ed824cb1d9e
Hannes Uhlin Collmo <148004074+hanino87@users.noreply.github.com>
2025-09-28 22:32:59 +0200

    uppdate products to product to get right api endpoints for creating products in the API

--- application/frontend/vite.config.js
+++ application/frontend/vite.config.js
@@ -6,6 +6,6 @@ export default defineConfig({
   server: {
     host: true,
     port: 5173,
-    allowedHosts: ['app-frontend', 'localhost'] // ✅ allow both local + Jenkins
+    allowedHosts: ['app-frontend', 'localhost'] // ✅ allow both local + Jenkins this makes jenkins to accept app-frontend vite dont get 403 not allowed 
   }
 })
